688dbc2d9a9156a44427fbb8acbaede4aeb20f4d
Lukaru <leonidastee1@gmail.com>
2025-05-17 13:02:23 +0800

    Simple movement script + Stickman sprite

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] float moveSpeed = 2f;
+
+	Rigidbody2D rb;
+	Vector2 movement;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+	}
+
+	void FixedUpdate()
+	{
+		// move the player using physics
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+}
