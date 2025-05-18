83cea1384cba0ec67fcdf917822c853cead7dbe1
Lukaru <leonidastee1@gmail.com>
2025-05-18 20:31:24 +0800

    Select and Use cards

--- Assets/Scripts/Cards/AbstractCard.cs
+++ Assets/Scripts/Cards/AbstractCard.cs
@@ -1,27 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class AbstractCard : MonoBehaviour
 {
 	Image image;
 
-	[SerializeField] float lerpAmount = 0.1f;
+	[SerializeField] float lerpAmount = 0.1f; // how fast to lerp to target position and angle
 	Vector3 targetPosition;
 	float targetAngle;
+
+	[SerializeField] float lerpLeeway = 0.01f; // how close should it lerp to before it snaps to target and stop lerping
+	bool shouldLerpPos;
+	bool shouldLerpRot;
 
 	void Awake()
 	{
 		image = GetComponent<Image>();
 		targetPosition.Set(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
-		targetAngle = 0;
+		targetAngle = transform.localRotation.eulerAngles.z;
+		shouldLerpPos = false;
+		shouldLerpRot = false;
 	}
 
 	void Update()
 	{
-		transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, lerpAmount);
-		transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, 0, targetAngle), lerpAmount);
+		if (shouldLerpPos)
+		{
+			// lerp position
+			if (Vector3.Distance(transform.localPosition, targetPosition) < lerpLeeway)
+			{
+				transform.localPosition = targetPosition;
+				shouldLerpPos = false;
+			}
+			else
+				transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, lerpAmount);
+		}
+
+		if (shouldLerpRot)
+		{
+			// lerp rotation
+			if (Mathf.Abs(transform.localRotation.eulerAngles.z - targetAngle) < lerpLeeway)
+			{
+				transform.localRotation = Quaternion.Euler(0, 0, targetAngle);
+				shouldLerpRot = false;
+			}
+			else
+				transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, 0, targetAngle), lerpAmount);
+		}
+	}
+
+	public virtual void UseCardEffect()
+	{
+		return;
+	}
+
+	public void DestroySelf()
+	{
+		// idealy do a 'use' animation before destroying
+		Destroy(gameObject);
 	}
 
 	public void SetColour(Color _color)
@@ -33,10 +69,12 @@ public class AbstractCard : MonoBehaviour
 	{
 		targetPosition.x = _x;
 		targetPosition.y = _y;
+		shouldLerpPos = true;
 	}
 
 	public void SetTargetRotation(float _z)
 	{
 		targetAngle = _z;
+		shouldLerpRot = true;
 	}
 }
--- Assets/Scripts/Cards/CardsManager.cs
+++ Assets/Scripts/Cards/CardsManager.cs
@@ -12,17 +12,66 @@ public class CardsManager : MonoBehaviour
 	[SerializeField] float cardTilt = 10f; // angle to tilt cards in hand
 	[SerializeField] float tiltOffsetDist = 200f; // distance to offset the cards depending on angle
 
+	[SerializeField] float selectedCardY = 150; // added yPos of selected card
+
 	List<AbstractCard> cardsInHand;
+
+	int selectedCard;
 
 
 	void Awake()
 	{
 		cardsInHand = new List<AbstractCard>();
+		selectedCard = 0;
 	}
 
 	void Update()
 	{
-		
+		// switch cards with scroll wheel
+		if (cardsInHand.Count > 0)
+		{
+			float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+			if (scroll > 0f) // scroll up
+			{
+				int oldCard = selectedCard;
+				selectedCard = (cardsInHand.Count + selectedCard - 1) % cardsInHand.Count;
+
+				OrganizeCard(oldCard);
+				OrganizeCard(selectedCard);
+			}
+			else if (scroll < 0f) // scroll down
+			{
+				int oldCard = selectedCard;
+				selectedCard = (selectedCard + 1) % cardsInHand.Count;
+
+				OrganizeCard(oldCard);
+				OrganizeCard(selectedCard);
+			}
+		}
+
+		// use selected card with space
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			if (selectedCard >= 0 && selectedCard < cardsInHand.Count)
+			{
+				AbstractCard card = cardsInHand[selectedCard];
+				if (card != null)
+				{
+					card.UseCardEffect();
+					cardsInHand.RemoveAt(selectedCard);
+					card.DestroySelf();
+
+					if (cardsInHand.Count > 0)
+					{
+						selectedCard %= cardsInHand.Count;
+						OrganizeHand();
+					}
+					else
+						selectedCard = 0;
+				}
+			}
+		}
 	}
 
 	public void GenerateCardInHand()
@@ -39,12 +88,21 @@ public class CardsManager : MonoBehaviour
 	void OrganizeHand()
 	{
 		for (int i = 0; i < cardsInHand.Count; ++i)
-		{
-			AbstractCard card = cardsInHand[i];
+			OrganizeCard(i);
+	}
 
-			float angle = cardTilt * ((float)(cardsInHand.Count - 1) / 2 - i);
-			card.SetTargetPosition(-xDistApart * (cardsInHand.Count - i - 1) + Mathf.Sin(Mathf.Deg2Rad * angle) * tiltOffsetDist, (Mathf.Cos(Mathf.Deg2Rad * angle) - 1) * tiltOffsetDist);
-			card.SetTargetRotation(angle);
+	void OrganizeCard(int _cardNo)
+	{
+		if (_cardNo >= 0 && _cardNo < cardsInHand.Count)
+		{
+			AbstractCard card = cardsInHand[_cardNo];
+			if (card != null)
+			{
+				float angle = cardTilt * ((float)(cardsInHand.Count - 1) / 2 - _cardNo);
+				card.SetTargetPosition(-xDistApart * (cardsInHand.Count - _cardNo - 1) + Mathf.Sin(Mathf.Deg2Rad * angle) * tiltOffsetDist,
+					(Mathf.Cos(Mathf.Deg2Rad * angle) - 1) * tiltOffsetDist + (_cardNo == selectedCard ? selectedCardY : 0));
+				card.SetTargetRotation(_cardNo == selectedCard ? 0 : angle);
+			}
 		}
 	}
 }
