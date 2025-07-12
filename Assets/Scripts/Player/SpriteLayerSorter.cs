39fea55d55f40b2261922583fc47458145417dd0
Lukaru <leonidastee1@gmail.com>
2025-07-12 21:23:34 +0800

    Layering Sprites by Y

---
+++ Assets/Scripts/Player/SpriteLayerSorter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SpriteLayerSorter : MonoBehaviour
+{
+	[SerializeField, Tooltip("Null = find during Awake")] SpriteRenderer spriteRenderer;
+	[SerializeField, Tooltip("Origin position of the sprite for depth calculation")] float yOffset = 0f;
+
+	void Awake()
+	{
+		if (spriteRenderer == null)
+			spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void LateUpdate()
+	{
+		spriteRenderer.sortingOrder = Mathf.RoundToInt((transform.position.y + yOffset) * -100);
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawSphere(transform.position + Vector3.up*yOffset, 0.01f);
+	}
+}
