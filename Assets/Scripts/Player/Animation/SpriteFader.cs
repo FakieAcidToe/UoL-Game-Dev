96eaf21cfb4cfaa16f07b5246d7ab03443cee707
Lukaru <leonidastee1@gmail.com>
2025-08-04 17:28:09 +0800

    Laser Weapon

---
+++ Assets/Scripts/Player/Animation/SpriteFader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteFader : MonoBehaviour
+{
+	[Header("Fade Properties")]
+	[SerializeField] float fadeSpeed = -1f;
+	[SerializeField, Range(0f, 1f)] float minFade = 0f;
+	[SerializeField, Range(0f, 1f)] float maxFade = 1f;
+
+	SpriteRenderer sr;
+
+	void Awake()
+	{
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, Mathf.Clamp(sr.color.a + fadeSpeed * Time.deltaTime, minFade, maxFade));
+	}
+}
