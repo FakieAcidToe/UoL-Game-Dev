8b2016a9a849615a74b73a0cfc9c9a3bb9c4e624
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2025-08-06 17:54:51 +0800

    Add skeleton for leveling system

---
+++ Assets/Scripts/Pickups/DropRateManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropRateManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Drops
+    {
+        public string name;
+        public GameObject itemPrefab;
+        public float dropRate;
+    }
+
+    public List<Drops> drops;
+    bool isQuitting = false;
+
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
+
+    void OnDestroy()
+    {
+        if (isQuitting)
+            return;
+        float randomNumber = UnityEngine.Random.Range(0f, 100f);
+        List<Drops> possibleDrops = new List<Drops>();
+
+        foreach (Drops rate in drops)
+        {
+            if (randomNumber <= rate.dropRate)
+            {
+                possibleDrops.Add(rate);
+            }
+        }
+        // Check the drop pool for possible drops
+        if (possibleDrops.Count > 0)
+        {
+            Drops drops = possibleDrops[UnityEngine.Random.Range(0, possibleDrops.Count)];
+            Instantiate(drops.itemPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerExperience.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerExperience : MonoBehaviour
+{
+    // Player Experience and Level
+    [Header("Experience/Level")]
+    public int experience = 0;
+    public int level = 1;
+    public int experienceCap;
+
+    // Class to define level range and the corresponding experience cap increase
+    [System.Serializable]
+    public class LevelRange
+    {
+        public int startLevel;
+        public int endLevel;
+        public int experienceCapIncrease;
+    }
+
+    public List<LevelRange> levelRanges;
+
+    void Start()
+    {
+        // Initialise experienceCap as the first experience cap
+        experienceCap = levelRanges[0].experienceCapIncrease;
+    }
+
+    public void IncreaseExperience(int amount)
+    {
+        experience += amount;
+        LevelUpChecker();
+    }
+
+    void LevelUpChecker()
+    {
+        if (experience > experienceCap)
+        {
+            level++;
+            experience = experienceCap;
+
+            int experienceCapIncrease = 0;
+            foreach (LevelRange range in levelRanges)
+            {
+                if (level >= range.startLevel && level <= range.endLevel)
+                {
+                    experienceCapIncrease = range.experienceCapIncrease;
+                    break;
+                }
+            }
+            experienceCap += experienceCapIncrease;
+        }
+    }
+}
