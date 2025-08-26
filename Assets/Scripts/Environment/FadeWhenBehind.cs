5631c8714cf5144fbf712b7de7c421bb8656e767
Lukaru <leonidastee1@gmail.com>
2025-08-26 18:22:08 +0800

    HDB Block

---
+++ Assets/Scripts/Environment/FadeWhenBehind.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class FadeWhenBehind : MonoBehaviour
+{
+	[SerializeField] float fadedAlpha = 0f;
+	[SerializeField] float fadeSpeed = 5f;
+	SpriteRenderer spriteRenderer;
+	float targetAlpha = 1f;
+	Color originalColor;
+
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		originalColor = spriteRenderer.color;
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+			targetAlpha = fadedAlpha;
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+			targetAlpha = 1f;
+	}
+
+	void Update()
+	{
+		spriteRenderer.color = new Color(originalColor.r, originalColor.g, originalColor.b,
+			Mathf.Lerp(spriteRenderer.color.a, targetAlpha, Time.deltaTime * fadeSpeed));
+	}
+}
