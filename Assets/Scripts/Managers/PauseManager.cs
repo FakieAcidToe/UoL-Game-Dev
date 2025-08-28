17825c9f756154aa13c408e0751cb4511d2f5ebd
Lukaru <leonidastee1@gmail.com>
2025-08-28 15:44:49 +0800

    Pause Screen

---
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+	[SerializeField] GameObject pauseMenuUI;
+	bool isPaused = false;
+
+	void Awake()
+	{
+		Time.timeScale = 1f;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+			PauseButton();
+	}
+
+	public void PauseButton()
+	{
+		if (isPaused) Resume();
+		else Pause();
+	}
+
+	public void Resume()
+	{
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		isPaused = false;
+	}
+
+	public void Pause()
+	{
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		isPaused = true;
+	}
+}
