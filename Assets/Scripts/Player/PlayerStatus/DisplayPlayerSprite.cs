0512c193f5658681dc8eed582be1e2b2703b4c17
Lukaru <leonidastee1@gmail.com>
2025-08-28 18:36:56 +0800

    Animate player sprite in Upgrade scene

---
+++ Assets/Scripts/Player/PlayerStatus/DisplayPlayerSprite.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayPlayerSprite : MonoBehaviour
+{
+	[SerializeField] Image playerImg;
+	[SerializeField] float animSpeed = 0.25f;
+
+	Sprite[] playerAnimation;
+
+	void Start()
+	{
+		if (PlayerStatus.Instance != null && PlayerStatus.Instance.selectedCharacter != null)
+			playerAnimation = new Sprite[] { PlayerStatus.Instance.selectedCharacter.animationSet.idleSide1, PlayerStatus.Instance.selectedCharacter.animationSet.idleSide2 };
+	}
+
+	void Update()
+	{
+		if (playerAnimation != null)
+		{
+			playerImg.sprite = playerAnimation[Mathf.FloorToInt(Time.timeSinceLevelLoad / animSpeed) % playerAnimation.Length];
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerStatus/DisplayStat.cs
+++ Assets/Scripts/Player/PlayerStatus/DisplayStat.cs
@@ -1,42 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class DisplayStat : MonoBehaviour
 {
-    public Text hpText;
-    public Text atkText;
-    public Text defText;
-    public Text critText;
-    public Text spdText;
-    public Text purText;
-    public Text atkspdText;
-    public Text cooldownText;
-    public Text expText;
-    public Text pointText;
-    public Text currentPointText;
+	[SerializeField] Text hpText;
+	[SerializeField] Text atkText;
+	[SerializeField] Text defText;
+	[SerializeField] Text critText;
+	[SerializeField] Text spdText;
+	[SerializeField] Text purText;
+	[SerializeField] Text atkspdText;
+	[SerializeField] Text cooldownText;
+	[SerializeField] Text expText;
+	[SerializeField] Text pointText;
+	[SerializeField] Text currentPointText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        hpText.text = string.Format("{0,-12}: {1}", "HP", PlayerStatus.Instance.playerHP);
-        atkText.text = string.Format("{0,-12}: {1}", "Attack", PlayerStatus.Instance.playerAtk);
-        defText.text = string.Format("{0,-12}: {1}", "Defense", PlayerStatus.Instance.playerDef);
-        critText.text = string.Format("{0,-12}: {1}", "CRIT Rate", PlayerStatus.Instance.playerCrit + "%");       
-        spdText.text = string.Format("{0,-12}: {1}", "Movespeed", (PlayerStatus.Instance.playerSPD * 100)+ "%");
-        purText.text = string.Format("{0,-12}: {1}", "Pick Up Range", (PlayerStatus.Instance.playerPUR * 100) + "%");
-        atkspdText.text = string.Format("{0,-12}: {1}", "Attack Speed", (PlayerStatus.Instance.playerAS * 100) + "%");
-        cooldownText.text = string.Format("{0,-12}: {1}", "Cooldown", ((100 - (PlayerStatus.Instance.playerCD * 100)) + "%"));
-        expText.text = string.Format("{0,-12}: {1}", "EXP rate", (int)(PlayerStatus.Instance.playerEXP * 100) + "%");
-        pointText.text = string.Format("{0,-12}: {1}", "Point rate", (int)(PlayerStatus.Instance.playerPoint * 100) + "%");
-        currentPointText.text = string.Format("{0,-12}: {1}", "UP Points", PlayerStatus.Instance.playerUpgradePoints);
-    }
+	void Update()
+	{
+		hpText.text = string.Format("{0,-12}: {1}", "HP", PlayerStatus.Instance.playerHP);
+		atkText.text = string.Format("{0,-12}: {1}", "Attack", PlayerStatus.Instance.playerAtk);
+		defText.text = string.Format("{0,-12}: {1}", "Defense", PlayerStatus.Instance.playerDef);
+		critText.text = string.Format("{0,-12}: {1}", "CRIT Rate", PlayerStatus.Instance.playerCrit + "%");       
+		spdText.text = string.Format("{0,-12}: {1}", "Movespeed", (PlayerStatus.Instance.playerSPD * 100)+ "%");
+		purText.text = string.Format("{0,-12}: {1}", "Pick Up Range", (PlayerStatus.Instance.playerPUR * 100) + "%");
+		atkspdText.text = string.Format("{0,-12}: {1}", "Attack Speed", (PlayerStatus.Instance.playerAS * 100) + "%");
+		cooldownText.text = string.Format("{0,-12}: {1}", "Cooldown", ((100 - (PlayerStatus.Instance.playerCD * 100)) + "%"));
+		expText.text = string.Format("{0,-12}: {1}", "EXP rate", (int)(PlayerStatus.Instance.playerEXP * 100) + "%");
+		pointText.text = string.Format("{0,-12}: {1}", "Point rate", (int)(PlayerStatus.Instance.playerPoint * 100) + "%");
+		currentPointText.text = string.Format("{0,-12}: {1}", "UP Points", PlayerStatus.Instance.playerUpgradePoints);
+	}
 }
