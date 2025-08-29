b0127cfd2c2c674d940bf893753f583d855e76e4
KH <nakanghou@gmail.com>
2025-08-29 16:18:41 +0800

    fixed some issues

--- Assets/Scripts/Player/PlayerStatus/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus/PlayerStatus.cs
@@ -7,9 +7,6 @@ public class PlayerStatus : MonoBehaviour
 
 	public float playerDMGB = 1;
 	public int dmgbUpgrades = 0;
-
-	public float playerDMG = 1; // note: not implemented yet
-	public int ATKUpgrades = 0;
 	
 	public int playerHP = 100;
 	public int hpUpgrades= 0;
@@ -64,7 +61,7 @@ public class PlayerStatus : MonoBehaviour
 		switch (stat)
 		{
 			case "ATK":
-				return ATKUpgrades;
+				return dmgbUpgrades;
 			case "HP":
 				return hpUpgrades;
 			case "CRIT":
