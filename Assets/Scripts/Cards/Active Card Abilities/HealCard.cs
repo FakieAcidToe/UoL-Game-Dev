e3b47266d772760b329ea6efb53262d23827faba
Lukaru <leonidastee1@gmail.com>
2025-08-29 21:00:50 +0800

    Heal Card

---
+++ Assets/Scripts/Cards/Active Card Abilities/HealCard.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class HealCard : ActiveCard
+{
+	[Header("Heal Card Properties")]
+	[SerializeField, Min(0)] int[] healAmount;
+	[SerializeField, TextArea] string[] upgradeBlurbs;
+
+	public override void UseCardEffect()
+	{
+		if (CanBeUsed())
+		{
+			playerObj.GetComponent<PlayerHP>().TakeDamage(-GetCurrentHealAmount());
+			TriggerCooldown();
+		}
+	}
+
+	int GetCurrentHealAmount()
+	{
+		return healAmount[Mathf.Clamp(GetDupeTimes() - 1, 0, healAmount.Length - 1)];
+	}
+
+	public override int GetMaxDupeTimes()
+	{
+		return healAmount.Length;
+	}
+
+	public override string GetBlurb()
+	{
+		int i = GetDupeTimes();
+		if (i >= 0 && i < upgradeBlurbs.Length)
+			return upgradeBlurbs[i];
+		return "";
+	}
+}
--- Assets/Scripts/Cards/Active Card Abilities/TurretCard.cs
+++ Assets/Scripts/Cards/Active Card Abilities/TurretCard.cs
@@ -3,7 +3,8 @@
 public class TurretCard : ActiveCard
 {
 	[Header("Turret Card Properties")]
-	[SerializeField] ProjectileSpawner turretPrefab;
+	[SerializeField] ProjectileSpawner[] turretPrefab;
+	[SerializeField, TextArea] string[] upgradeBlurbs;
 	ProjectileSpawner spawnedTurret;
 
 	public override void UseCardEffect()
@@ -11,8 +12,34 @@ public class TurretCard : ActiveCard
 		if (CanBeUsed())
 		{
 			if (spawnedTurret != null) Destroy(spawnedTurret.gameObject);
-			spawnedTurret = Instantiate(turretPrefab, playerObj.position, Quaternion.identity);
+			spawnedTurret = Instantiate(GetCurrentPrefab(), playerObj.position, Quaternion.identity);
 			TriggerCooldown();
 		}
+	}
+
+	public override void DestroySelf()
+	{
+		if (spawnedTurret != null)
+			Destroy(spawnedTurret.gameObject);
+
+		base.DestroySelf();
+	}
+
+	ProjectileSpawner GetCurrentPrefab()
+	{
+		return turretPrefab[Mathf.Clamp(GetDupeTimes() - 1, 0, turretPrefab.Length - 1)];
+	}
+
+	public override int GetMaxDupeTimes()
+	{
+		return turretPrefab.Length;
+	}
+
+	public override string GetBlurb()
+	{
+		int i = GetDupeTimes();
+		if (i >= 0 && i < upgradeBlurbs.Length)
+			return upgradeBlurbs[i];
+		return "";
 	}
 }
