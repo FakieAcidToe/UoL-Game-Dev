f9067ad13f20a761a98c25bc4f2b7b9d9bad3fa3
Lukaru <leonidastee1@gmail.com>
2025-08-31 16:04:15 +0800

    Animate title background

---
+++ Assets/Scripts/UI/ImagePlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImagePlayer : MonoBehaviour
+{
+	[Header("Sprite Properties")]
+	[SerializeField, Tooltip("Hitbox sprite set")] Sprite[] sprites;
+	[SerializeField, Tooltip("Time (seconds) per sprite"), Min(0)] float animationSpeed = 0.06f;
+
+	// sprites
+	Image image;
+	float animationTimer = 0f;
+	int imageIndex = 0;
+
+	void Awake()
+	{
+		image = GetComponent<Image>();
+		if (sprites.Length > 0)
+			image.sprite = sprites[0];
+	}
+
+	void Update()
+	{
+		if (sprites.Length > 0 && animationSpeed > 0f)
+		{
+			animationTimer += Time.deltaTime;
+			if (animationTimer > animationSpeed)
+			{
+				animationTimer -= animationSpeed;
+				imageIndex = (imageIndex + 1) % sprites.Length;
+				image.sprite = sprites[imageIndex];
+			}
+		}
+	}
+}
